d865026baa0e782943aee4d51491f62af77791ae
raghavian <raghav@di.ku.dk>
2023-03-23 21:46:19 +0100

    Make OFA surrogate dataset

---
+++ make_ofadataset.py
@@ -0,0 +1,32 @@
+import json
+import numpy as np
+import torch
+import pdb
+
+DFILE='/home/raghav/erda/projects/surrogate_ofa/results.json'
+
+with open(DFILE) as jfile:
+    data = json.load(jfile)
+
+N = len(data)
+print('Found dataset with %d entries'%N)
+d = len(data[0]['d']) 
+e = len(data[0]['e']) 
+w = len(data[0]['w']) 
+D = d+e+w+2
+ofaX = torch.zeros((N,D))
+ofaY = torch.zeros(N)
+
+for i in range(N):
+#    pdb.set_trace()
+    ofaX[i,:d] = torch.Tensor(np.array(data[i]['d'],dtype=float)).reshape(-1)
+    ofaX[i,d:d+e] = torch.Tensor(np.array(data[i]['e'],dtype=float)).reshape(-1)
+    ofaX[i,d+e:d+e+w] = torch.Tensor(np.array(data[i]['w'],dtype=float)).reshape(-1)
+    ofaX[i,-2] = torch.Tensor([float(data[i]['flops'])])
+    ofaX[i,-1] = torch.Tensor([float(data[i]['params'])])
+
+    ofaY[i] = torch.Tensor([float(data[i]['energy'])])
+
+torch.save((ofaX,ofaY),'ofa_%d.pt'%N)
+
+
